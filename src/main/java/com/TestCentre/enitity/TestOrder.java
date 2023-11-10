7e7ae676c12ce919fd9418b87d578e9434d284dd
Deepanwita2000 <85988268+Deepanwita2000@users.noreply.github.com>
2023-11-10 23:47:38 +0530

    Add files via upload

---
+++ src/main/java/com/TestCentre/enitity/TestOrder.java
@@ -0,0 +1,57 @@
+package com.TestCentre.enitity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class TestOrder {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String doctor;
+	private String test;
+	public TestOrder(int id, String name, String doctor, String test) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.doctor = doctor;
+		this.test = test;
+	}
+	public TestOrder() {
+		super();
+		
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDoctor() {
+		return doctor;
+	}
+	public void setDoctor(String doctor) {
+		this.doctor = doctor;
+	}
+	public String getTest() {
+		return test;
+	}
+	public void setTest(String test) {
+		this.test = test;
+	}
+	
+	
+	
+	
+	
+
+}
